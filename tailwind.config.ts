7ab52d650cae9736fbcf1bdf4eba7b158ccacc35
tsotne-ch <tchavtchavadze.ts@icloud.com>
2024-11-01 20:06:39 +0400

    Refresh

--- tailwind.config.ts
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./resources/**/*.{html,templ,js,ts}", 'node_modules/preline/dist/*.js'],
-  theme: {
-    extend: {},
-  },
-  plugins: [require('preline/plugin')],
-}
-
